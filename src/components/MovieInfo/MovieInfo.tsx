cb0cbb0500896f098271a533fd2f89242e993705
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-01-04 19:15:37 +0100

    Rating from MovieInfo

--- src/components/MovieInfo/MovieInfo.tsx
+++ src/components/MovieInfo/MovieInfo.tsx
@@ -1,4 +1,5 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useContext } from 'react';
+import API from '../../API';
 
 //Styles
 import { Wrapper, Content, Text } from './MovieInfo.styles';
@@ -16,6 +17,9 @@ import { Movie, Crew } from '../../types';
 //Components
 import Rate from '../Rate/Rate'
 
+//Context
+import { Context } from '../../context'
+
 
 interface MovieInfoProps {
     movie: Movie & {
@@ -23,43 +27,55 @@ interface MovieInfoProps {
     }
 }
 
-const MovieInfo: FunctionComponent<MovieInfoProps> = ({movie}) => (
-    <Wrapper backdrop={movie.backdrop_path}>
-        <Content>
-            <Thumb 
-                image={
-                    movie.poster_path
-                    ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
-                    : NoImage
-                }
-                clickable={false} 
-                movieId={movie.id}
-            />
-            <Text>
-                <h1>{movie.title}</h1>
-                <h3>PLOT</h3>
-                <p>{movie.overview}</p>
+const MovieInfo: FunctionComponent<MovieInfoProps> = ({ movie }) => {
+    //@ts-ignore
+    const [user] = useContext(Context);
 
-                <div className='rating-directors'>
-                    <div>
-                        <h3>RATING</h3>
-                        <div className='score'>{movie.vote_average}</div>
+    const handleRating = async (value: number) => {
+        const rate = await API.rateMovie(user.sessionId, movie.id, value);
+        console.log(rate)
+    }
+
+    return (
+        <Wrapper backdrop={movie.backdrop_path}>
+            <Content>
+                <Thumb
+                    image={
+                        movie.poster_path
+                            ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+                            : NoImage
+                    }
+                    clickable={false}
+                    movieId={movie.id}
+                />
+                <Text>
+                    <h1>{movie.title}</h1>
+                    <h3>PLOT</h3>
+                    <p>{movie.overview}</p>
+
+                    <div className='rating-directors'>
+                        <div>
+                            <h3>RATING</h3>
+                            <div className='score'>{movie.vote_average}</div>
+                        </div>
+                        <div className='director'>
+                            <h3>DIRECTOR{movie.directors.length > 1 ? 'S' : ''}</h3>
+                            {movie.directors.map(director => (
+                                <p key={director.credit_id}>{director.name}</p>
+                            ))}
+                        </div>
                     </div>
-                    <div className='director'>
-                        <h3>DIRECTOR{movie.directors.length > 1 ? 'S' : ''}</h3>
-                        {movie.directors.map(director => (
-                            <p key={director.credit_id}>{director.name}</p>
-                        ))}
-                    </div>
-                </div>
-                <div>
-                    <p>Rate movie</p>
-                    <Rate/>
-                </div>
-            </Text>
-        </Content>
-    </Wrapper>
-);
+                    {user &&
+                        (<div>
+                            <p>Rate movie</p>
+                            <Rate callback={handleRating}/>
+                        </div>)
+                    }
+                </Text>
+            </Content>
+        </Wrapper>
+    );
+}
 
 
 export default MovieInfo
